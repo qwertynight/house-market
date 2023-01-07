1c95b8afb4c0c7625823127f0fb064ab0854886a
Alihan Ruslanov <alihanruslanov@MacBook-Pro-Alihan.local>
2023-01-07 17:18:02 -0600

    changed

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,15 +1,14 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBSGAUWzJzCjXsQWytt8InNoV-8Przfziw",
-	authDomain: "test-371323.firebaseapp.com",
-	projectId: "test-371323",
-	storageBucket: "test-371323.appspot.com",
-	messagingSenderId: "837906810736",
-	appId: "1:837906810736:web:a7cf294a6a571f3435f1b5",
-	measurementId: "G-6QM46W244X"
-	}
+  apiKey: "AIzaSyDFxggJ8qyOFey_Ihsg_G6AE0bRsRu02Cc",
+  authDomain: "house-market-44557.firebaseapp.com",
+  projectId: "house-market-44557",
+  storageBucket: "house-market-44557.appspot.com",
+  messagingSenderId: "235610066373",
+  appId: "1:235610066373:web:b791c4a9e81083636f0fac",
+};
 
-const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
